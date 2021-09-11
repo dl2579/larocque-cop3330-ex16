ab1ca91b6b5da0a5808c92c6897eda563c780843
dl2579 <damianlarocque257@gmail.com>
2021-09-10 23:01:54 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Damian LaRocque
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print( "What is your age? " );
+        int age = input.nextInt();
+
+        String output = age>=16 ? "You are old enough to legally drive." : "You are not old enough to legally drive.";
+
+        System.out.println(output);
+
+    }
+}
